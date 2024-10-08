e0b59b1d4845fcecd07e62368461bf6b4d034077
Fabi Tureo <fabianestebantureo@gmail.com>
2024-10-08 20:02:44 -0300

    fix: replace list

--- src/Pages/AboutUs/CardList.jsx
+++ src/Pages/AboutUs/CardList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import CardItem from './CardItem';
-import './CardItem.css';
+import './cardItem.css';
 
 const CardList = ({ cards }) => {
   return (
